a0e323a874c91820243125897e5c3e3023c180b9
wang-bin <wbsecg1@gmail.com>
2018-05-12 12:33:34 +0800

    win: support cross build for windows using clang-cl+lld

--- windows.clang.cmake
+++ windows.clang.cmake
@@ -7,7 +7,16 @@
 #
 # clang-cl + lld to cross build apps for windows. can be easily change to other target platforms
 # can not use clang --target=${ARCH}-none-windows-msvc19.0 because cmake will test msvc flags
-# -Xclang clang options
+# ref: https://github.com/llvm-mirror/llvm/blob/master/cmake/platforms/WinMsvc.cmake
+
+# vars:
+# WINSDK_DIR, WINSDK_VER, MSVC_DIR. If not set, environment vars WindowsSdkDir, WindowsSDKVersion, VCDIR are used
+# CMAKE_SYSTEM_PROCESSOR: target arch, host arch is used if not set
+# CMAKE_C_COMPILER: clang-cl path (optional)
+
+# when cross building on a case sensitive filesystem, symbolic links for libs and vfs overlay for headers are required.
+# You can download winsdk containing scripts to generate links and vfs overlay from: https://sourceforge.net/projects/avbuild/files/dep/winsdk.7z/download
+# msvc sdk: https://sourceforge.net/projects/avbuild/files/dep/msvcrt-dev.7z/download
 
 # /bin/link will be selected by cmake
 # non-windows host: clang-cl invokes link.exe by default, use -fuse-ld=lld works. but -Wl, /link, -Xlinker does not work
@@ -18,161 +27,110 @@ option(UWP "build for uwp" OFF)
 option(PHONE "build for phone" OFF)
 option(ONECORE "build with oncore" OFF)
 
-set(CMAKE_CROSSCOMPILING ON) # turned on by setting CMAKE_SYSTEM_NAME
+set(CMAKE_CROSSCOMPILING ON) # turned on by setting CMAKE_SYSTEM_NAME?
+# FIXME: msvcrtd.lib is required
+set(CMAKE_SYSTEM_NAME Windows) # phone, store?
+#set(CMAKE_SYSTEM_VERSION 10.0)
+
 if(NOT CMAKE_C_COMPILER)
-  set(CMAKE_C_COMPILER clang-cl)
-  set(CMAKE_CXX_COMPILER clang-cl)
-endif()
-if(NOT CLANG_AS_LINKER)
-  set(CMAKE_LINKER lld-link  CACHE INTERNAL "lld linker" FORCE)
+  set(CMAKE_C_COMPILER clang-cl CACHE FILEPATH "")
+  set(CMAKE_CXX_COMPILER clang-cl CACHE FILEPATH "")
+  set(CMAKE_LINKER lld-link CACHE FILEPATH "")
+# llvm-ar is not required to create static lib: lld-link /lib /machine:${WINSDK_ARCH}
 endif()
 
 if(NOT CMAKE_SYSTEM_PROCESSOR)
-  set(CMAKE_SYSTEM_PROCESSOR x86)
+  message("CMAKE_SYSTEM_PROCESSOR for target is not set. Must be aarch64(arm64), armv7(arm), x86(i686), x64(x86_64). Assumeme build for host arch: ${CMAKE_HOST_SYSTEM_PROCESSOR}.")
+  set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
 endif()
-if(CMAKE_SYSTEM_PROCESSOR MATCHES "x.*64")
-  set(TARGET_TRIPLE_ARCH x86_64)
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "x.*64" OR CMAKE_SYSTEM_PROCESSOR STREQUAL AMD64)
+  set(TRIPLE_ARCH x86_64)
+  set(WINSDK_ARCH x64)
 elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "86")
-  set(TARGET_TRIPLE_ARCH i386)
-  set(LIB_ARCH x86)
+  set(TRIPLE_ARCH i386)
+  set(WINSDK_ARCH x86)
 elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "a.*64")
-  set(TARGET_TRIPLE_ARCH arm64)
+  set(TRIPLE_ARCH aarch64)
+  set(WINSDK_ARCH arm64)
 elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "arm")
-  set(TARGET_TRIPLE_ARCH armv7)
+  set(TRIPLE_ARCH armv7)
+  set(WINSDK_ARCH arm)
 endif()
-if(NOT LIB_ARCH)
-  set(LIB_ARCH ${CMAKE_SYSTEM_PROCESSOR})
+
+# fetch env vars set by vcvarsall.bat if required vars are not set
+if(NOT WINSDK_DIR)
+  set(WINSDK_DIR $ENV{WindowsSdkDir})
 endif()
-set(CMAKE_SYSTEM_NAME Windows) # assume host build if not set, host flags will be used, e.g. apple clang flags are added on macOS
-if(UWP)
-  set(CMAKE_SYSTEM_NAME WINDOWS_STORE)
-  set(WINRT 1)
+if(NOT WINSDK_VER)
+  set(WINSDK_VER $ENV{WindowsSDKVersion})
 endif()
-if(PHONE)
-  set(CMAKE_SYSTEM_NAME WINDOWS_PHONE)
-  set(WINRT 1)
+if(NOT MSVC_DIR)
+  set(MSVC_DIR $ENV{VCDIR})
 endif()
-if(WINRT)
-  set(WINSTORE 1)
+# Export configurable variables for the try_compile() command. Or set env var like llvm
+set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES
+  CMAKE_SYSTEM_PROCESSOR
+  MSVC_DIR
+  WINSDK_DIR
+  WINSDK_VER
+)
+
+set(MSVC_INCLUDE "${MSVC_DIR}/include")
+set(MSVC_LIB "${MSVC_DIR}/lib")
+set(WINSDK_INCLUDE "${WINSDK_DIR}/Include/${WINSDK_VER}")
+set(WINSDK_LIB "${WINSDK_DIR}/Lib/${WINSDK_VER}")
+
+if(NOT EXISTS "${WINSDK_INCLUDE}/um/Windows.h")
+  message(SEND_ERROR "Cannot find Windows.h")
 endif()
-if(NOT CMAKE_SYSTEM_VERSION)
-  if(UWP)
-    set(CMAKE_SYSTEM_VERSION 6.3)
-  elseif(WINRT OR ONECORE)
-    set(CMAKE_SYSTEM_VERSION 6.2)
-  else()
-    set(CMAKE_SYSTEM_VERSION 5.1)
+
+set(COMPILE_FLAGS
+    -D_CRT_SECURE_NO_WARNINGS
+    --target=${TRIPLE_ARCH}-windows-msvc
+    -fms-compatibility-version=19.14)
+
+if(NOT CMAKE_HOST_WIN32) # assume CMAKE_HOST_WIN32 means in VS env, vs tools like rc and mt exists
+  if(NOT EXISTS "${WINSDK_INCLUDE}/um/WINDOWS.H")
+    set(case_sensitive_fs TRUE)
   endif()
+  if(case_sensitive_fs)
+    if(NOT EXISTS "${WINSDK_DIR}/vfs.yaml")
+      message(SEND_ERROR "can not find vfs.yaml. you can use winsdk from https://sourceforge.net/projects/avbuild/files/dep/winsdk.7z/download")
+    endif()
+    list(APPEND COMPILE_FLAGS -Xclang -ivfsoverlay -Xclang "${WINSDK_DIR}/vfs.yaml")
+  endif()
+  list(APPEND COMPILE_FLAGS
+    -imsvc "${MSVC_INCLUDE}"
+    -imsvc "${WINSDK_INCLUDE}/ucrt"
+    -imsvc "${WINSDK_INCLUDE}/shared"
+    -imsvc "${WINSDK_INCLUDE}/um"
+    -imsvc "${WINSDK_INCLUDE}/winrt")
+
+  list(APPEND LINK_FLAGS
+    # Prevent CMake from attempting to invoke mt.exe. It only recognizes the slashed form and not the dashed form.
+    /manifest:no # why -manifest:no results in rc error?  TODO: check mt and rc?
+    -libpath:"${MSVC_LIB}/${WINSDK_ARCH}"
+    -libpath:"${WINSDK_LIB}/ucrt/${WINSDK_ARCH}"
+    -libpath:"${WINSDK_LIB}/um/${WINSDK_ARCH}")
 endif()
 
-set(USE_STD_TLS ON)
+string(REPLACE ";" " " COMPILE_FLAGS "${COMPILE_FLAGS}")
+set(CMAKE_C_FLAGS "${COMPILE_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${COMPILE_FLAGS}" CACHE STRING "" FORCE)
 
-# flags for both compiler and linker
-if(USE_CLANG_CL)
-# /std:<value>
-  set(WIN_FLAGS "-fms-compatibility -fms-extensions")
-endif()
-set(WIN_FLAGS "${WIN_FLAGS} --target=${TARGET_TRIPLE_ARCH}-none-windows-msvc19.13.0 -MD") # /arch:${ARCH} has no effect?
-#set(WIN_FLAGS "${WIN_FLAGS} -triple ${TARGET_TRIPLE_ARCH}-none-windows-msvc19.13.0 -MD") # /arch:${ARCH} has no effect?
-#set(WIN_FLAGS "${WIN_FLAGS} -nostdinc") # skip clang include dir. FIXME: will omit /imsvc options
-set(WIN_FLAGS_CXX)
+string(REPLACE ";" " " LINK_FLAGS "${LINK_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${LINK_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_MODULE_LINKER_FLAGS "${LINK_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_SHARED_LINKER_FLAGS "${LINK_FLAGS}" CACHE STRING "" FORCE)
 
-# Sysroot.
-if(NOT WindowsSdkDir)
-  set(WindowsSdkDir $ENV{WindowsSdkDir})
-endif()
-if(NOT WindowsSDKVersion)
-  set(WindowsSDKVersion $ENV{WindowsSDKVersion})
-endif()
-if(NOT WindowsSDKVersion)
-  set(WindowsSDKVersion 10.0.16299.0)
-endif()
-if(NOT VCDIR)
-  set(VCDIR $ENV{VCDIR})
-endif()
+# CMake populates these with a bunch of unnecessary libraries, which requires
+# extra case-correcting symlinks and what not. Instead, let projects explicitly
+# control which libraries they require.
+set(CMAKE_C_STANDARD_LIBRARIES "" CACHE STRING "" FORCE)
+set(CMAKE_CXX_STANDARD_LIBRARIES "" CACHE STRING "" FORCE)
 
-#set(CMAKE_SYSROOT ${WIN_SYSROOT})
-
-# llvm-ranlib is for bitcode. but seems works for others. "llvm-ar -s" should be better
-# macOS system ranlib does not work
-set(CMAKE_RANLIB llvm-ranlib)
-set(CMAKE_LLVM_AR llvm-ar)
-# llvm-ar for all host platforms. support all kinds of file, including bitcode
-get_filename_component(LLVM_DIR ${CMAKE_RANLIB} DIRECTORY) #clang-cl -v: InstalledDir: /usr/local/opt/llvm/bin
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
-# CMake 3.9 tries to use CMAKE_SYSROOT_COMPILE before it gets set from CMAKE_SYSROOT, which leads to using the system's /usr/include. Set this manually.
-# https://github.com/android-ndk/ndk/issues/467
-if(CMAKE_SYSROOT)
-  set(CMAKE_SYSROOT_COMPILE "${CMAKE_SYSROOT}")
-endif()
-#set(WIN_CC_FLAGS "-g")
-# Debug and release flags.
-#set(WIN_CC_FLAGS_DEBUG "-O0 -fno-limit-debug-info")
-#set(WIN_CC_FLAGS_RELEASE "-O2 -DNDEBUG")
-
-
-if(NOT CMAKE_HOST_WIN32)
-# llvm-rc error on windows. no effect on other hosts
 set(CMAKE_RC_COMPILER_INIT llvm-rc CACHE INTERNAL "windows llvm rc" FORCE)
 set(CMAKE_RC_COMPLIER llvm-rc CACHE INTERNAL "windows llvm rc" FORCE)
 set(CMAKE_GENERATOR_RC llvm-rc CACHE INTERNAL "windows llvm rc" FORCE)
-
-  if(USE_LIBCXX)
-    set(WIN_FLAGS_CXX "${WIN_FLAGS_CXX} -fdelayed-template-parsing -stdlib=libc++") # for both compiler & linker
-    # clang generates __cxa_thread_atexit for thread_local, but armhf libc++abi is too old. linking to supc++, libstdc++ results in duplicated symbols when linking static libc++. so never link to supc++. rename to glibc has __cxa_thread_atexit_impl!
-  else() # gcc files can be found by clang
-    add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>")
-  endif()
-
-  add_compile_options("/imsvc ${VCDIR}/include")
-  foreach(m IN ITEMS shared ucrt um winrt)
-    add_compile_options("/imsvc ${WindowsSdkDir}/Include/${WindowsSDKVersion}/${m}")
-  endforeach()
-  link_libraries(/libpath:${VCDIR}/lib/${LIB_ARCH})
-  foreach(m IN ITEMS ucrt um)
-    link_libraries(/libpath:${WindowsSdkDir}/Lib/${WindowsSDKVersion}/${m}/${LIB_ARCH})
-  endforeach()
-  macro(WIN_cc_clang lang)
-    set(CMAKE_${lang}_LINK_EXECUTABLE
-      "<CMAKE_LINKER> <CMAKE_${lang}_LINK_FLAGS> <LINK_FLAGS> <LINK_LIBRARIES> <OBJECTS> -o <TARGET>")                
-    set(CMAKE_${lang}_CREATE_SHARED_LIBRARY
-      "<CMAKE_LINKER> <CMAKE_${lang}_LINK_FLAGS> <CMAKE_SHARED_LIBRARY_${lang}_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_${lang}_FLAGS> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
-    set(CMAKE_${lang}_CREATE_SHARED_MODULE
-      "<CMAKE_LINKER> <CMAKE_${lang}_LINK_FLAGS> <CMAKE_SHARED_MODULE_${lang}_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_MODULE_CREATE_${lang}_FLAGS> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
-  endmacro()
-
-  # clang-cl: /U... is treated as option /U, use -- to treat as file
-  # FIXME: MUST change /usr/local/share/cmake/Modules//Platform/Windows-MSVC.cmake +286
-  macro(__windows_compiler_clang_cl lang)
-    set(CMAKE_${lang}_COMPILE_OBJECT
-      "<CMAKE_${lang}_COMPILER> ${CMAKE_START_TEMP_FILE} ${CMAKE_CL_NOLOGO}${_COMPILE_${lang}} <DEFINES> <INCLUDES> <FLAGS> /Fo<OBJECT> /Fd<TARGET_COMPILE_PDB>${_FS_${lang}} -c -- <SOURCE>${CMAKE_END_TEMP_FILE}"
-      CACHE INTERNAL "clang-cl ${lang} command" FORCE)
-  endmacro()
-  if(USE_CLANG_CL)
-    __windows_compiler_clang_cl(C)
-    __windows_compiler_clang_cl(CXX)
-  endif()
-  if(NOT CLANG_AS_LINKER)
-    WIN_cc_clang(C)
-    WIN_cc_clang(CXX)
-  endif()
-endif()
-if(CLANG_AS_LINKER)
-  link_libraries(-Wl,--build-id -fuse-ld=lld) # -s: strip
-else()
-  #set(CMAKE_LINER      "lld" CACHE INTERNAL "linker" FORCE)
-  set(WIN_LD_FLAGS "${WIN_LD_FLAGS} --build-id")# --sysroot=${CMAKE_SYSROOT}") # -s: strip
-
-endif()
-#53472, 5702912
-# Set or retrieve the cached flags. Without these compiler probing may fail!
-
-set(CMAKE_AR         "${CMAKE_LLVM_AR}" CACHE INTERNAL "windows llvm ar" FORCE)
-set(CMAKE_C_FLAGS    "${WIN_FLAGS}" CACHE INTERNAL "windows llvm c compiler flags" FORCE)
-set(CMAKE_CXX_FLAGS  "${WIN_FLAGS} ${WIN_FLAGS_CXX}"  CACHE INTERNAL "windows llvm c++ compiler/linker flags" FORCE)
-set(CMAKE_ASM_FLAGS  "${WIN_FLAGS}"  CACHE INTERNAL "windows llvm asm compiler flags" FORCE)
+# Allow clang-cl to work with macOS paths.
+set(CMAKE_USER_MAKE_RULES_OVERRIDE "${CMAKE_CURRENT_LIST_DIR}/override.windows.clang.cmake")
