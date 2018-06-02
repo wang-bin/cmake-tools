90a1599459add70c1e7bf052baa9f1a05f2fbc67
wang-bin <wbsecg1@gmail.com>
2018-06-02 19:44:24 +0800

    unify toolchain files for linux

---
+++ linux.clang.cmake
@@ -0,0 +1,122 @@
+# This file is part of the cmake-tools project. It was retrieved from
+# https://github.com/wang-bin/cmake-tools
+#
+# The cmake-tools project is licensed under the new MIT license.
+#
+# Copyright (c) 2017-2018, Wang Bin
+#
+# clang + lld to cross build apps for linux
+#
+
+option(CLANG_AS_LINKER "use clang as linker to invoke lld. MUST ON for now" ON)
+option(USE_LIBCXX "use libc++ instead of libstdc++" OFF)
+option(USE_LIBCXXABI "use libc++abi. required by some platforms" OFF)
+option(USE_TARGET_LIBCXX "libc++ headers bundled with clang are searched and used by default. usually safe if abi is stable. set to true to use target libc++ if version is different" OFF)
+option(USE_STD_TLS "use std c++11 thread_local" OFF) # sunxi ubuntu12.04 libc is too old to have __cxa_thread_atexit_impl
+option(LINUX_FLAGS "flags for both compiler and linker, e.g. --target=arm-rpi-linux-gnueabihf ..." "")
+
+set(CMAKE_SYSTEM_NAME Linux) # assume host build if not set, host flags will be used, e.g. apple clang flags are added on macOS
+# "/usr/local/opt/llvm/bin/ld.lld" --sysroot=/Users/wangbin/dev/rpi/sysroot -pie -X --eh-frame-hdr -m armelf_linux_eabi -dynamic-linker /lib/ld-linux-armhf.so.3 -o test/audiodec /Users/wangbin/dev/rpi/sysroot/usr/lib/../lib/Scrt1.o /Users/wangbin/dev/rpi/sysroot/usr/lib/../lib/crti.o /Users/wangbin/dev/rpi/sysroot/lib/../lib/crtbeginS.o -L/Users/wangbin/dev/rpi/sysroot/lib/../lib -L/Users/wangbin/dev/rpi/sysroot/usr/lib/../lib -L/Users/wangbin/dev/rpi/sysroot/lib -L/Users/wangbin/dev/rpi/sysroot/usr/lib --build-id --as-needed --gc-sections --enable-new-dtags -z origin "-rpath=\$ORIGIN" "-rpath=\$ORIGIN/lib" -rpath-link /Users/wangbin/dev/multimedia/mdk/external/lib/rpi/armv6 test/CMakeFiles/audiodec.dir/audiodec.cpp.o libmdk.so.0.1.0 -lc++ -lm -lgcc_s -lgcc -lc -lgcc_s -lgcc /Users/wangbin/dev/rpi/sysroot/lib/../lib/crtendS.o /Users/wangbin/dev/rpi/sysroot/usr/lib/../lib/crtn.o
+
+# Export configurable variables for the try_compile() command. Or set env var like llvm
+set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES
+  CMAKE_SYSTEM_PROCESSOR
+)
+
+if(CMAKE_CROSSCOMPILING)
+  set(CMAKE_C_COMPILER clang-6.0)
+  set(CMAKE_CXX_COMPILER clang++-6.0)
+else()
+  set(CMAKE_C_COMPILER clang)
+  set(CMAKE_CXX_COMPILER clang++)
+endif()
+
+# llvm-ranlib is for bitcode. but seems works for others. "llvm-ar -s" should be better
+# macOS system ranlib does not work
+execute_process(
+  COMMAND ${CMAKE_C_COMPILER} -print-prog-name=llvm-ranlib
+  OUTPUT_VARIABLE CMAKE_RANLIB
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+# llvm-ar for all host platforms. support all kinds of file, including bitcode
+execute_process(
+  COMMAND ${CMAKE_C_COMPILER} -print-prog-name=llvm-ar
+  OUTPUT_VARIABLE CMAKE_LLVM_AR
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+get_filename_component(LLVM_DIR ${CMAKE_RANLIB} DIRECTORY)
+
+# Sysroot.
+set(CMAKE_SYSROOT ${LINUX_SYSROOT})
+file(GLOB_RECURSE PKGCONFIG_DIRS LIST_DIRECTORIES true "${CMAKE_SYSROOT}/usr/lib/*pkgconfig*") # pkgconfig is dir, so LIST_DIRECTORIES must be true (false by default for GLOB_RECURSE)
+list(FILTER PKGCONFIG_DIRS INCLUDE REGEX "/pkgconfig")
+string(REPLACE ";" ":" PKGCONFIG_DIRS "${PKGCONFIG_DIRS}")
+if(CMAKE_CROSSCOMPILING)
+  set(ENV{PKG_CONFIG_PATH} "${CMAKE_SYSROOT}/usr/share/pkgconfig:${PKGCONFIG_DIRS}")
+endif()
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# CMake 3.9 tries to use CMAKE_SYSROOT_COMPILE before it gets set from CMAKE_SYSROOT, which leads to using the system's /usr/include. Set this manually.
+# https://github.com/android-ndk/ndk/issues/467
+set(CMAKE_SYSROOT_COMPILE "${CMAKE_SYSROOT}")
+
+set(LINUX_CC_FLAGS "-g")
+# Debug and release flags.
+set(LINUX_CC_FLAGS_DEBUG "-O0 -fno-limit-debug-info")
+set(LINUX_CC_FLAGS_RELEASE "-O2 -DNDEBUG")
+
+if(USE_LIBCXX)
+  if(CMAKE_CROSSCOMPILING AND USE_TARGET_LIBCXX) # assume libc++ abi is stable, then USE_TARGET_LIBCXX=0 is ok, i.e. build with host libc++, but run with a different target libc++ version
+  # headers in clang builtin include dir(stddef.h etc.). -nobuiltininc makes cross build harder if a header is not found in sysroot(include_next stddef.h in /usr/include/linux/)
+    # -nostdinc++: clang always search libc++(-stdlib=libc++) in host toolchain, may mismatch with target libc++ version, and results in conflict(include_next)
+    if(CMAKE_VERSION VERSION_LESS 3.3)
+      set(LINUX_FLAGS_CXX "${LINUX_FLAGS_CXX} -nostdinc++ -iwithsysroot /usr/include/c++/v1")
+    else()
+      #add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>")
+      add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++;-iwithsysroot;/usr/include/c++/v1>")
+    endif()
+    # -stdlib=libc++ is not required if -nostdinc++ is set(otherwise warnings)
+    link_libraries(-stdlib=libc++) #unlike SUNXI_LD_FLAGS, it will append flags to last
+  else()
+    set(LINUX_FLAGS_CXX "${LINUX_FLAGS_CXX} -stdlib=libc++") # for both compiler & linker
+  endif()
+  if(USE_LIBCXXABI)
+    link_libraries(-lc++abi)
+  endif()
+  # clang generates __cxa_thread_atexit for thread_local, but armhf libc++abi is too old. linking to supc++, libstdc++ results in duplicated symbols when linking static libc++. so never link to supc++. rename to glibc has __cxa_thread_atexit_impl!
+# link to libc++abi?
+  if(USE_STD_TLS)
+    link_libraries(-Wl,-defsym,__cxa_thread_atexit=__cxa_thread_atexit_impl)
+  endif()
+  #link_libraries(-lsupc++)
+else() # gcc files can be found by clang
+endif()
+
+if(CLANG_AS_LINKER)
+  link_libraries(-Wl,--build-id -fuse-ld=lld) # -s: strip
+else()
+  #set(CMAKE_LINER      "lld" CACHE INTERNAL "linker" FORCE)
+  set(LINUX_LD_FLAGS "${LINUX_LD_FLAGS} --build-id --sysroot=${CMAKE_SYSROOT}") # -s: strip
+  macro(set_cc_clang lang)
+    set(CMAKE_${lang}_LINK_EXECUTABLE
+        "<CMAKE_LINKER> -flavor gnu <CMAKE_${lang}_LINK_FLAGS> <LINK_FLAGS> <LINK_LIBRARIES> <OBJECTS> -o <TARGET>")                
+    set(CMAKE_${lang}_CREATE_SHARED_LIBRARY
+        "<CMAKE_LINKER> -flavor gnu <CMAKE_${lang}_LINK_FLAGS> <CMAKE_SHARED_LIBRARY_${lang}_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_${lang}_FLAGS> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
+    set(CMAKE_${lang}_CREATE_SHARED_MODULE
+        "<CMAKE_LINKER> -flavor gnu <CMAKE_${lang}_LINK_FLAGS> <CMAKE_SHARED_MODULE_${lang}_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_MODULE_CREATE_${lang}_FLAGS> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
+  endmacro()
+  set_cc_clang(C)
+  set_cc_clang(CXX)
+endif()
+#53472, 5702912
+# Set or retrieve the cached flags. Without these compiler probing may fail!
+
+set(CMAKE_AR         "${CMAKE_LLVM_AR}" CACHE INTERNAL "${CMAKE_SYSTEM_NAME} ar" FORCE)
+set(CMAKE_C_FLAGS    "${LINUX_FLAGS}" CACHE INTERNAL "${CMAKE_SYSTEM_NAME} c compiler flags" FORCE)
+set(CMAKE_CXX_FLAGS  "${LINUX_FLAGS} ${LINUX_FLAGS_CXX}"  CACHE INTERNAL "${CMAKE_SYSTEM_NAME} c++ compiler/linker flags" FORCE)
+set(CMAKE_ASM_FLAGS  "${LINUX_FLAGS}"  CACHE INTERNAL "${CMAKE_SYSTEM_NAME} asm compiler flags" FORCE)
+
+set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
--- rpi.clang.cmake
+++ rpi.clang.cmake
@@ -10,11 +10,20 @@
 # Options:
 # - RPI_SYSROOT or env var RPI_SYSROOT
 # - USE_LIBCXX
-
+# TODO: xlocale.h: https://stackoverflow.com/questions/24738059/c-error-locale-t-has-not-been-declared, https://sourceware.org/bugzilla/show_bug.cgi?id=10456
 # FIXME: g++8 libm lgammaf32_r@GLIBC_2.27 is missing on stretch(libavcodec/libavformat)
-option(CLANG_AS_LINKER "use clang as linker to invoke lld. MUST ON for now" ON)
-option(USE_LIBCXX "use libc++ instead of libstdc++" OFF)
-# "/usr/local/opt/llvm/bin/ld.lld" --sysroot=/Users/wangbin/dev/rpi/sysroot -pie -X --eh-frame-hdr -m armelf_linux_eabi -dynamic-linker /lib/ld-linux-armhf.so.3 -o test/audiodec /Users/wangbin/dev/rpi/sysroot/usr/lib/../lib/Scrt1.o /Users/wangbin/dev/rpi/sysroot/usr/lib/../lib/crti.o /Users/wangbin/dev/rpi/sysroot/lib/../lib/crtbeginS.o -L/Users/wangbin/dev/rpi/sysroot/lib/../lib -L/Users/wangbin/dev/rpi/sysroot/usr/lib/../lib -L/Users/wangbin/dev/rpi/sysroot/lib -L/Users/wangbin/dev/rpi/sysroot/usr/lib --build-id --as-needed --gc-sections --enable-new-dtags -z origin "-rpath=\$ORIGIN" "-rpath=\$ORIGIN/lib" -rpath-link /Users/wangbin/dev/multimedia/mdk/external/lib/rpi/armv6 test/CMakeFiles/audiodec.dir/audiodec.cpp.o libmdk.so.0.1.0 -lc++ -lm -lgcc_s -lgcc -lc -lgcc_s -lgcc /Users/wangbin/dev/rpi/sysroot/lib/../lib/crtendS.o /Users/wangbin/dev/rpi/sysroot/usr/lib/../lib/crtn.o
+# mini sysroot: https://sourceforge.net/projects/avbuild/files/raspberry-pi/rpi-sysroot.tar.xz/download
+
+set(CMAKE_SYSTEM_PROCESSOR armv6)
+set(RPI 1)
+set(OS rpi)
+set(LINUX_SYSROOT "${RPI_SYSROOT}")
+if(NOT LINUX_SYSROOT OR NOT EXISTS "${LINUX_SYSROOT}")
+  set(LINUX_SYSROOT "$ENV{RPI_SYSROOT}")
+endif()
+
+# flags for both compiler and linker
+set(LINUX_FLAGS "--target=arm-rpi-linux-gnueabihf -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -mfpu=vfp -marm")
 
 if(EXISTS /dev/vchiq)
   set(CMAKE_CROSSCOMPILING OFF)
@@ -22,106 +31,8 @@ else()
   set(CMAKE_CROSSCOMPILING ON)
 endif()
 
-set(CMAKE_SYSTEM_NAME Linux) # assume host build if not set, host flags will be used, e.g. apple clang flags are added on macOS
-#set(CMAKE_SYSTEM_VERSION 1)
-set(CMAKE_SYSTEM_PROCESSOR armv6)
-if(CMAKE_CROSSCOMPILING)
-  set(CMAKE_C_COMPILER clang-6.0)
-  set(CMAKE_CXX_COMPILER clang++-6.0)
-else()
-  set(CMAKE_C_COMPILER clang)
-  set(CMAKE_CXX_COMPILER clang++)
-endif()
-
+# set options in linux.clang.cmake
 set(USE_STD_TLS ON)
-
-# flags for both compiler and linker
-set(RPI_FLAGS "--target=arm-rpi-linux-gnueabihf -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -mfpu=vfp -marm")
-set(RPI_FLAGS_CXX)
-
-# Sysroot.
-if(NOT RPI_SYSROOT)
-  set(RPI_SYSROOT $ENV{RPI_SYSROOT})
-endif()
-set(CMAKE_SYSROOT ${RPI_SYSROOT})
-if(CMAKE_CROSSCOMPILING)
-  set(ENV{PKG_CONFIG_PATH} ${CMAKE_SYSROOT}/usr/lib/arm-linux-gnueabihf/pkgconfig)
-endif()
-
-# llvm-ranlib is for bitcode. but seems works for others. "llvm-ar -s" should be better
-# macOS system ranlib does not work
-execute_process(
-  COMMAND ${CMAKE_C_COMPILER} -print-prog-name=llvm-ranlib
-  OUTPUT_VARIABLE CMAKE_RANLIB
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-# llvm-ar for all host platforms. support all kinds of file, including bitcode
-execute_process(
-  COMMAND ${CMAKE_C_COMPILER} -print-prog-name=llvm-ar
-  OUTPUT_VARIABLE CMAKE_LLVM_AR
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-get_filename_component(LLVM_DIR ${CMAKE_RANLIB} DIRECTORY)
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
-# CMake 3.9 tries to use CMAKE_SYSROOT_COMPILE before it gets set from CMAKE_SYSROOT, which leads to using the system's /usr/include. Set this manually.
-# https://github.com/android-ndk/ndk/issues/467
-set(CMAKE_SYSROOT_COMPILE "${CMAKE_SYSROOT}")
-
-set(RPI_CC_FLAGS "-g")
-# Debug and release flags.
-set(RPI_CC_FLAGS_DEBUG "-O0 -fno-limit-debug-info")
-set(RPI_CC_FLAGS_RELEASE "-O2 -DNDEBUG")
-
-if(USE_LIBCXX)
-  if(CMAKE_CROSSCOMPILING AND USE_TARGET_LIBCXX) # assume libc++ abi is stable, then USE_TARGET_LIBCXX=0 is ok, i.e. build with host libc++, but run with a different target libc++ version
-  # headers in clang builtin include dir(stddef.h etc.). -nobuiltininc makes cross build harder if a header is not found in sysroot(include_next stddef.h in /usr/include/linux/)
-    # -nostdinc++: clang always search libc++(-stdlib=libc++) in host toolchain, may mismatch with target libc++ version, and results in conflict(include_next)
-    if(CMAKE_VERSION VERSION_LESS 3.3)
-      set(RPI_FLAGS_CXX "${RPI_FLAGS_CXX} -nostdinc++ -iwithsysroot /usr/include/c++/v1")
-    else()
-      #add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>")
-      add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++;-iwithsysroot;/usr/include/c++/v1>")
-    endif()
-    # -stdlib=libc++ is not required if -nostdinc++ is set(otherwise warnings)
-    link_libraries(-stdlib=libc++) #unlike RPI_LD_FLAGS, it will append flags to last
-  else()
-    set(RPI_FLAGS_CXX "${RPI_FLAGS_CXX} -stdlib=libc++") # for both compiler & linker
-  endif()
-  # clang generates __cxa_thread_atexit for thread_local, but armhf libc++abi is too old. linking to supc++, libstdc++ results in duplicated symbols when linking static libc++. so never link to supc++. rename to glibc has __cxa_thread_atexit_impl!
-# link to libc++abi?
-  link_libraries(-Wl,-defsym,__cxa_thread_atexit=__cxa_thread_atexit_impl)
-  #link_libraries(-lsupc++)
-else() # gcc files can be found by clang
-endif()
-
-macro(rpi_cc_clang lang)
-  set(CMAKE_${lang}_LINK_EXECUTABLE
-    "<CMAKE_LINKER> -flavor gnu <CMAKE_${lang}_LINK_FLAGS> <LINK_FLAGS> <LINK_LIBRARIES> <OBJECTS> -o <TARGET>")                
-  set(CMAKE_${lang}_CREATE_SHARED_LIBRARY
-    "<CMAKE_LINKER> -flavor gnu <CMAKE_${lang}_LINK_FLAGS> <CMAKE_SHARED_LIBRARY_${lang}_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_${lang}_FLAGS> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
-  set(CMAKE_${lang}_CREATE_SHARED_MODULE
-    "<CMAKE_LINKER> -flavor gnu <CMAKE_${lang}_LINK_FLAGS> <CMAKE_SHARED_MODULE_${lang}_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_MODULE_CREATE_${lang}_FLAGS> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
-endmacro()
-
-if(CLANG_AS_LINKER)
-  link_libraries(-Wl,--build-id -fuse-ld=lld) # -s: strip
-else()
-  #set(CMAKE_LINER      "lld" CACHE INTERNAL "linker" FORCE)
-  set(RPI_LD_FLAGS "${RPI_LD_FLAGS} --build-id --sysroot=${CMAKE_SYSROOT}") # -s: strip
-  rpi_cc_clang(C)
-  rpi_cc_clang(CXX)
-endif()
-#53472, 5702912
-# Set or retrieve the cached flags. Without these compiler probing may fail!
-
-set(CMAKE_AR         "${CMAKE_LLVM_AR}" CACHE INTERNAL "rpi ar" FORCE)
-set(CMAKE_C_FLAGS    "${RPI_FLAGS}" CACHE INTERNAL "rpi c compiler flags" FORCE)
-set(CMAKE_CXX_FLAGS  "${RPI_FLAGS} ${RPI_FLAGS_CXX}"  CACHE INTERNAL "rpi c++ compiler/linker flags" FORCE)
-set(CMAKE_ASM_FLAGS  "${RPI_FLAGS}"  CACHE INTERNAL "rpi asm compiler flags" FORCE)
-
-set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
+set(USE_LIBCXX OFF)
+set(USE_LIBCXXABI OFF)
+include(${CMAKE_CURRENT_LIST_DIR}/linux.clang.cmake)
